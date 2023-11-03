ed99a65e6a8c5eb7bd3280b827795c38a62bbae0
Henry Babbage <hbabbage@gmail.com>
2023-11-03 18:48:19 +0100

    sort out font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,12 @@ module.exports = {
       ],
       theme: {
         extend: {
+            screens: {
+				sm: '640px',
+				md: '768px',
+				lg: '1024px',
+				xl: '1440px',
+			},
             colors: {
 				primary: '#222222',
 				secondary: '#BFBFBF',
@@ -17,6 +23,12 @@ module.exports = {
             fontFamily: {
 				serif: ['var(--font-serif)', ...fontFamily.serif],
 			},
+            fontSize: {
+                sm: ['14px', '20px'],
+                base: ['16px', '24px'],
+                lg: ['18px', '24px'],
+                xl: ['20px', '24px'],
+            },
 			zIndex: {
 				100: '100',
 				200: '200',
@@ -28,6 +40,41 @@ module.exports = {
 				800: '800',
 				900: '900',
 				1000: '1000',
+			},
+            keyframes: {
+				'accordion-down': {
+					from: { height: 0 },
+					to: { height: 'var(--radix-accordion-content-height)' },
+				},
+				'accordion-up': {
+					from: { height: 'var(--radix-accordion-content-height)' },
+					to: { height: 0 },
+				},
+				'fade-in': {
+					'0%': {
+						opacity: '0',
+					},
+					'100%': {
+						opacity: '1',
+					},
+				},
+				'scale-in': {
+					'0%': {
+						opacity: '0',
+						transform: 'scale(0)',
+					},
+					'100%': {
+						opacity: '1',
+						transform: 'scale(1)',
+					},
+				},
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out',
+				'scale-in': 'scale-in 0.2s ease-out',
+				'slide-in': 'slide-in 1s ease-in',
+				'fade-in': 'fade-in 0.5s ease-in-out',
 			},
         },
       },
