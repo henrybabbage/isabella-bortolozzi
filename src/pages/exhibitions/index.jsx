1ec8a66dce670ac8814b1d062d9fac657d38191a
Henry Babbage <hbabbage@gmail.com>
2023-11-03 12:24:10 +0100

    add missing import

--- src/pages/exhibitions/index.jsx
+++ src/pages/exhibitions/index.jsx
@@ -2,6 +2,7 @@ import { useLiveQuery } from 'next-sanity/preview'
 import { Client } from 'react-hydration-provider'
 
 import TableView from '@/components/Common/TableView'
+import { readToken } from '@/lib/sanity.api'
 import { getClient } from '@/lib/sanity.client'
 import { getExhibitions } from '@/lib/sanity.fetch'
 import { exhibitionsQuery } from '@/lib/sanity.queries'
