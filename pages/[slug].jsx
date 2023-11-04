9ccc73379367bfbe48ab75fe697e33aeca648da4
Henry Babbage <hbabbage@gmail.com>
2023-11-04 14:38:38 +0100

    fix imports

--- pages/[slug].jsx
+++ pages/[slug].jsx
@@ -1,11 +1,12 @@
-import { readToken } from 'lib/sanity.api'
-import { getClient } from 'lib/sanity.client'
+import { useLiveQuery } from 'next-sanity/preview'
+
+import { readToken } from '@/lib/sanity.api'
+import { getClient } from '@/lib/sanity.client'
 import {
     artistBySlugQuery,
     artistSlugsQuery,
     getArtist,
-} from 'lib/sanity.queries'
-import { useLiveQuery } from 'next-sanity/preview'
+} from '@/lib/sanity.queries'
 
 export const getStaticProps = async ({ draftMode = false, params = {} }) => {
     const client = getClient(draftMode ? { token: readToken } : undefined)
