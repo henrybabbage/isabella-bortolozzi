3e05890c1ccdfaf7e786c49dee61201038591699
Henry Babbage <hbabbage@gmail.com>
2023-11-06 14:51:41 +0100

    add nuka carousel

--- components/ArtistPage/CarouselSection.jsx
+++ components/ArtistPage/CarouselSection.jsx
@@ -1,12 +1,25 @@
+import Carousel from "nuka-carousel"
+
 import { useActiveSectionStore } from '@/context/useActiveSectionStore'
 import { useSectionInView } from '@/hooks/useSectionInView'
+
+import SlideImage from "./SlideImage"
 
 export default function CarouselSection({ artist, isLoading }) {
 	const { ref } = useSectionInView("Carousel", 0.5)
   	const { setActiveSection } = useActiveSectionStore()
+
+    const imageGallery = artist?.imageGallery ?? []
+    console.log(imageGallery)
 	return (
 		<section ref={ref} id="carousel" className="relative h-screen w-screen flex flex-col items-center justify-center">
-			Carousel
+			<div className="h-3/4 w-3/4 relative bg-secondary">
+                <Carousel animation="fade" enableKeyboardControls swiping wrapAround withoutControls>
+                    {imageGallery && imageGallery.map((image, idx) => (
+                        <SlideImage image={image} key={idx} />
+                    ))}
+                </Carousel>
+            </div>
 		</section>
 	)
 }
--- pages/[slug].jsx
+++ pages/[slug].jsx
@@ -29,7 +29,7 @@ export default function ArtistSlugRoute(props) {
     return (
         <main className='flex flex-col h-full w-screen relative gap-24'>
             <ArtistSubNav artist={artist} isLoading={isLoading} />
-            <CarouselSection  artist={artist} isLoading={isLoading} />
+            <CarouselSection artist={artist} isLoading={isLoading} />
             <ExhibitionsSection artist={artist} />
             <CVSection artist={artist} />
         </main>
