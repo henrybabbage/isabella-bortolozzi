a0bf2a600366d6c6974eea0806b972552a0f91ed
Henry Babbage <hbabbage@gmail.com>
2023-11-06 12:13:35 +0100

    add missing import

--- hooks/useSectionInView.js
+++ hooks/useSectionInView.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 
+import { useActiveSectionStore } from "@/context/useActiveSectionStore";
+
 export function useSectionInView(sectionName, threshold = 0.75) {
     const { ref, inView } = useInView({
         threshold,
