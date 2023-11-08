f95b5a0ad1f32277b23e4aa511c1feead1ab6988
Henry Babbage <hbabbage@gmail.com>
2023-11-08 21:35:24 +0100

    fix animate slide in and add carousel drawer

--- components/Common/Drawers/ArtworkDrawer.jsx
+++ components/Common/Drawers/ArtworkDrawer.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 import { cn } from "@/utils/cn"
 
-export default function ArtworkDrawer({ setDrawerIsOpen, didClickPrevious, didClickNext, tabletOrMobile }) {
+export default function ArtworkDrawer({ drawerIsOpen, setDrawerIsOpen, didClickPrevious, didClickNext, tabletOrMobile }) {
     const [pressReleaseSelected, setPressReleaseSelected] = useState(false)
 
 	const togglePressRelease = () => {
@@ -16,9 +16,11 @@ export default function ArtworkDrawer({ setDrawerIsOpen, didClickPrevious, didCl
 
     return (
         <section
-			className={cn(tabletOrMobile ? 'static' : 'relative', 'pointer-events-auto z-100 flex h-full w-full flex-col place-content-end bg-background p-6')}
+			className={cn(
+                tabletOrMobile ? 'static' : 'relative',
+                drawerIsOpen ? 'opacity-100' : 'opacity-0',
+                'pointer-events-auto z-100 flex h-full w-full flex-col place-content-end bg-background p-6')}
 		>
-            <div></div>
             <div className="inline-flex space-x-[10px]">
                 <button onClick={didClickPrevious} className="cursor-pointer">
                     <h3 className="pointer-events-auto z-[999] text-secondary transition hover:text-primary">
