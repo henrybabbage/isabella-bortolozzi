71bdb48e1856862a73d6ac92f591fe3d9c049413
Henry Babbage <hbabbage@gmail.com>
2023-11-13 16:44:54 +0100

    add guard

--- components/ArtistPage/CVSection.jsx
+++ components/ArtistPage/CVSection.jsx
@@ -6,16 +6,19 @@ import ExhibitionList from './ExhibitionList'
 
 export default function CVSection({ artist }) {
     const { ref } = useSectionInView("biography", 0.1)
+
+    if (!artist) return null
+    
     return (
         <section ref={ref} id="biography" className="scroll-mt-20 h-screen min-h-screen px-6">
             <div className="grid grid-cols-12">
                 <div className="h-fit col-span-9 col-start-4">
                     <ArtistBio artistBio1={artist?.artistBio1} artistBio2={artist?.artistBio2} />
-                    <h3 className="mb-6 text-primary">Selected Solo Exhibitions</h3>
+                    {artist.soloExhibitionHistory && <h3 className="mb-6 text-primary">Selected Solo Exhibitions</h3>}
                     <div className='mb-6'>
                         {artist &&
-                            artist?.soloExhibitionHistory &&
-                            artist?.soloExhibitionHistory.map((exhibition, idx) => (
+                            artist.soloExhibitionHistory &&
+                            artist.soloExhibitionHistory.map((exhibition, idx) => (
                                 <ExhibitionList
                                     key={idx}
                                     year={exhibition?.year}
@@ -23,11 +26,11 @@ export default function CVSection({ artist }) {
                                 />
                         ))}
                     </div>
-                    <h3 className="mb-6 text-primary">Selected Group Exhibitions</h3>
+                    {artist.groupExhibitionHistory && <h3 className="mb-6 text-primary">Selected Group Exhibitions</h3>}
                     <div className='mb-6'>
                         {artist &&
-                            artist?.groupExhibitionHistory &&
-                            artist?.groupExhibitionHistory.map((exhibition, idx) => (
+                            artist.groupExhibitionHistory &&
+                            artist.groupExhibitionHistory.map((exhibition, idx) => (
                                 <ExhibitionList
                                     key={idx}
                                     year={exhibition?.year}
