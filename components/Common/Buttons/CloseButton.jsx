ee3d6efd730b5ef491a2f6005834f467f9e61c1d
Henry Babbage <hbabbage@gmail.com>
2023-11-14 22:49:23 +0100

    format buttons

--- components/Common/Buttons/CloseButton.jsx
+++ components/Common/Buttons/CloseButton.jsx
@@ -1,13 +1,23 @@
 import { Cross1Icon } from '@radix-ui/react-icons'
 import { forwardRef, useState } from 'react'
 
-const CloseButton = forwardRef(function CloseButton({ didPressButton }, ref={ref}) {
+const CloseButton = forwardRef(function CloseButton(
+  { didPressButton },
+  ref = { ref },
+) {
     const [isHovered, setIsHovered] = useState(false)
     const handleMouseEnter = () => setIsHovered(true)
     const handleMouseLeave = () => setIsHovered(false)
     const iconColor = isHovered ? '#BFBFBF' : '#222222'
     return (
-        <button type="button" aria-label='Close the open dialog' onClick={didPressButton} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="animate-fade-in animation-delay-[4000ms] w-6 h-6 flex justify-center items-center relative">
+        <button
+            type="button"
+            aria-label="Close the open dialog"
+            onClick={didPressButton}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className="animate-fade-in animation-delay-[4000ms] w-6 h-6 flex justify-center items-center relative"
+        >
             <Cross1Icon color={iconColor} className="absolute w-full h-full" />
         </button>
     )
--- components/Common/Buttons/PlusButton.jsx
+++ components/Common/Buttons/PlusButton.jsx
@@ -2,15 +2,23 @@ import { PlusIcon } from '@radix-ui/react-icons'
 import { forwardRef, useState } from 'react'
 
 const PlusButton = forwardRef(function PlusButton({ didPressButton }, ref) {
-    const [isHovered, setIsHovered] = useState(false)
-    const handleMouseEnter = () => setIsHovered(true)
-    const handleMouseLeave = () => setIsHovered(false)
-    const iconColor = isHovered ? '#BFBFBF' : '#222222'
-    return (
-        <button ref={ref} type="button" aria-label='View extra information related to the current image' onClick={didPressButton} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="animate-fade-in animation-delay-[4000ms] relative w-8 h-8 flex justify-center items-center">
-            <PlusIcon color={iconColor} className="absolute w-full h-full" />
-        </button>
-    )
+	const [isHovered, setIsHovered] = useState(false)
+	const handleMouseEnter = () => setIsHovered(true)
+	const handleMouseLeave = () => setIsHovered(false)
+	const iconColor = isHovered ? '#BFBFBF' : '#222222'
+	return (
+		<button
+			ref={ref}
+			type="button"
+			aria-label="View extra information related to the current image"
+			onClick={didPressButton}
+			onMouseEnter={handleMouseEnter}
+			onMouseLeave={handleMouseLeave}
+			className="animate-fade-in animation-delay-[4000ms] relative w-8 h-8 flex justify-center items-center"
+		>
+			<PlusIcon color={iconColor} className="absolute w-full h-full" />
+		</button>
+	)
 })
 
 export default PlusButton
