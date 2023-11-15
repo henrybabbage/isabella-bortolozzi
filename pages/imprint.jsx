e19feae1fd293dbebb10fd12b8fef81326020577
Henry Babbage <hbabbage@gmail.com>
2023-11-15 16:18:57 +0100

    tidy up

--- pages/imprint.jsx
+++ pages/imprint.jsx
@@ -10,29 +10,29 @@ export default function Imprint({ imprint }) {
         <main className="h-screen w-screen py-6 animate-fade-in">
             <Client>
                 <Desktop>
-                <section className='fixed h-auto z-50 top-0 grid w-screen grid-cols-12 px-6'>
+                    <section className='fixed h-auto z-50 top-0 grid w-screen grid-cols-12 px-6'>
                         <div className="col-span-5 col-start-4 flex w-full space-x-12 pt-6">
                             <h1 className="text-primary">Imprint</h1>
                         </div>
+                        </section>
+                        <section className="flex flex-col sm:grid sm:grid-cols-12 px-6 pb-6">
+                        {imprint.heading && (
+                            <div className="sm:col-span-5 sm:col-start-9">
+                                <CustomPortableText value={imprint.heading} />
+                            </div>
+                        )}
+                        {imprint.imprint && (
+                            <div className="sm:col-span-5 sm:col-start-9">
+                                <CustomPortableText value={imprint.imprint} />
+                            </div>
+                        )}
+                        {imprint.privacyPolicy && (
+                            <div className="sm:col-span-5 sm:col-start-9">
+                                <CustomPortableText value={imprint.privacyPolicy} />
+                            </div>
+                        )}
                     </section>
-                    <section className="flex flex-col sm:grid sm:grid-cols-12 px-6 pb-6">
-                    {imprint.heading && (
-                        <div className="sm:col-span-5 sm:col-start-9">
-                            <CustomPortableText value={imprint.heading} />
-                        </div>
-                    )}
-                    {imprint.imprint && (
-                        <div className="sm:col-span-5 sm:col-start-9">
-                            <CustomPortableText value={imprint.imprint} />
-                        </div>
-                    )}
-                    {imprint.privacyPolicy && (
-                        <div className="sm:col-span-5 sm:col-start-9">
-                            <CustomPortableText value={imprint.privacyPolicy} />
-                        </div>
-                    )}
-                    </section>
-                    </Desktop>
+                </Desktop>
                 <TabletAndBelow>
                     <div></div>
                 </TabletAndBelow>
