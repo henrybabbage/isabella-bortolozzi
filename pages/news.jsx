b36f4cb779159981f0504ca56facd0512ed5f1cd
Henry Babbage <hbabbage@gmail.com>
2023-11-29 21:22:14 +0100

    h-[100dvh]

--- pages/news.jsx
+++ pages/news.jsx
@@ -10,7 +10,7 @@ import { newsQuery } from '@/lib/sanity.queries'
 export default function News(props) {
     const [news] = useLiveQuery(props.news, newsQuery)
     return (
-        <main className="animate-fade-in h-screen w-screen">
+        <main className="animate-fade-in h-[100dvh] w-screen">
             <NewsHeader news={news} />
             <TableView exhibitions={news} />
         </main>
