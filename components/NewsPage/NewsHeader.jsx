3b606cb15a10271dd7e55e99cc78a2aca3a1d8bb
Henry Babbage <hbabbage@gmail.com>
2023-11-29 18:20:07 +0100

    news page responsiveness

--- components/NewsPage/NewsHeader.jsx
+++ components/NewsPage/NewsHeader.jsx
@@ -1,6 +1,6 @@
 export default function NewsHeader({ news }) {
     return (
-        <section className='fixed bg-background h-auto z-50 top-0 grid w-screen grid-cols-12 px-6'>
+        <section className='fixed bg-background h-auto z-50 top-0 grid w-screen grid-cols-12 px-6 pt-6 sm:pt-0 pb-1 sm:pb-0'>
             <div className="sm:col-span-9 sm:col-start-4 col-span-12 col-start-1 flex w-full space-x-12 pt-14 sm:pt-6">
                 <h1 className="text-primary">News</h1>
             </div>
