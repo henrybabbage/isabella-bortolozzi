990204436f6e5b0a3b43b1a7bf28c33a7e3f645b
Henry Babbage <hbabbage@gmail.com>
2023-11-29 18:21:35 +0100

    viewing rooms responsiveness in header

--- components/ViewingRoomsPage/ViewingRoomsHeader.jsx
+++ components/ViewingRoomsPage/ViewingRoomsHeader.jsx
@@ -1,7 +1,7 @@
 export default function ViewingRoomsHeader() {
     return (
-        <section className='fixed bg-background h-auto z-50 top-0 grid w-screen grid-cols-12 px-6'>
-            <div className="col-span-9 col-start-4 flex w-full space-x-12 pt-6">
+        <section className="fixed bg-background h-auto z-50 top-0 grid w-screen grid-cols-12 px-6 pt-6 pb-1 sm:pt-0 sm:pb-0">
+            <div className="sm:col-span-9 sm:col-start-4 col-span-12 col-start-1 flex justify-between sm:justify-normal w-full space-x-12 pt-14 sm:pt-6">
                 <h1 className="text-primary">Viewing Rooms</h1>
             </div>
         </section>
