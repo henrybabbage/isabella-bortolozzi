56c5dab95b4dbe46e39eaf0d51b93971f7b17fca
Henry Babbage <hbabbage@gmail.com>
2023-11-30 03:31:55 +0100

    test with nextjs-page-transition lib

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,14 @@
 import '@/styles/global.css'
+import '@madeinhaus/nextjs-page-transition/dist/index.css'
 
+import PageTransition, { useAsPathWithoutHash } from '@madeinhaus/nextjs-page-transition'
 import localFont from 'next/font/local'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import { lazy, useEffect, useMemo, useRef } from 'react'
+import { lazy, useEffect, useRef } from 'react'
 import { HydrationProvider } from 'react-hydration-provider'
 
 import RootLayout from '@/components/Layout/RootLayout'
-import useFoucFix from '@/hooks/useFoucFix'
 
 const PreviewProvider = lazy(() => import('@/components/Previews/PreviewProvider'))
 
@@ -29,85 +30,78 @@ const serif = localFont({
 
 export default function App({ Component, pageProps }) {
 	const { draftMode, token } = pageProps
-	
-	useFoucFix()
 
 	const router = useRouter()
+    const key = useAsPathWithoutHash()
 
-    const key = useMemo(() => {
-        const { pathname, query } = router
-        const slug = query.slug || ''
-        return `${pathname}-${slug}`
-    }, [router])
+	// const scrollCache = useRef({})
+	// const activeRestorePath = useRef()
 
-	const scrollCache = useRef({})
-	const activeRestorePath = useRef()
+	// useEffect(() => {
+	// 	if (history.scrollRestoration !== 'manual') {
+	// 		history.scrollRestoration = 'manual'
+	// 	}
+	// 	const getCurrentPath = () => location.pathname + location.search
+	// 	router.beforePopState(() => {
+	// 		activeRestorePath.current = getCurrentPath()
+	// 		return true
+	// 	})
+	// 	const onComplete = () => {
+    //         const scrollPath = activeRestorePath.current
+    //         if (!scrollPath || !(scrollPath in scrollCache.current)) {
+    //             window.scrollTo(0, 0)
+    //             return
+    //         }
 
-	useEffect(() => {
-		if (history.scrollRestoration !== 'manual') {
-			history.scrollRestoration = 'manual'
-		}
-		const getCurrentPath = () => location.pathname + location.search
-		router.beforePopState(() => {
-			activeRestorePath.current = getCurrentPath()
-			return true
-		})
-		const onComplete = () => {
-            const scrollPath = activeRestorePath.current
-            if (!scrollPath || !(scrollPath in scrollCache.current)) {
-                window.scrollTo(0, 0)
-                return
-            }
-
-            activeRestorePath.current = undefined
-            const [scrollX, scrollY] = scrollCache.current[scrollPath]
-            window.scrollTo(scrollX, scrollY)
-            // allow for page taking longer to load
-            const delays = [10, 20, 40, 80, 160]
-            const checkAndScroll = () => {
-                if (
-                    (window.scrollX === scrollX && window.scrollY === scrollY) ||
-                    scrollPath !== getCurrentPath()
-                ) {
-                    return
-                }
-                window.scrollTo(scrollX, scrollY)
-                const delay = delays.shift()
-                if (delay) {
-                    setTimeout(checkAndScroll, delay)
-                }
-            }
-            setTimeout(checkAndScroll, delays.shift())
-		}
-		const onScroll = () => {
-			scrollCache.current[getCurrentPath()] = [window.scrollX, window.scrollY]
-		}
-		router.events.on('routeChangeComplete', onComplete)
-		window.addEventListener('scroll', onScroll)
-		return () => {
-			router.events.off('routeChangeComplete', onComplete)
-			window.removeEventListener('scroll', onScroll)
-		}
-	}, [router])
+    //         activeRestorePath.current = undefined
+    //         const [scrollX, scrollY] = scrollCache.current[scrollPath]
+    //         window.scrollTo(scrollX, scrollY)
+    //         // allow for page taking longer to load
+    //         const delays = [10, 20, 40, 80, 160]
+    //         const checkAndScroll = () => {
+    //             if (
+    //                 (window.scrollX === scrollX && window.scrollY === scrollY) ||
+    //                 scrollPath !== getCurrentPath()
+    //             ) {
+    //                 return
+    //             }
+    //             window.scrollTo(scrollX, scrollY)
+    //             const delay = delays.shift()
+    //             if (delay) {
+    //                 setTimeout(checkAndScroll, delay)
+    //             }
+    //         }
+    //         setTimeout(checkAndScroll, delays.shift())
+	// 	}
+	// 	const onScroll = () => {
+	// 		scrollCache.current[getCurrentPath()] = [window.scrollX, window.scrollY]
+	// 	}
+	// 	router.events.on('routeChangeComplete', onComplete)
+	// 	window.addEventListener('scroll', onScroll)
+	// 	return () => {
+	// 		router.events.off('routeChangeComplete', onComplete)
+	// 		window.removeEventListener('scroll', onScroll)
+	// 	}
+	// }, [router])
 
     // add "scroll-smooth" class for navigation within a page but do not apply the class during page transitions
-    useEffect(() => {
-        const html = document.querySelector('html')
-        const addSmoothScrollClass = () => {
-            html.classList.add('scroll-smooth')
-        }
-        const removeSmoothScrollClass = () => {
-            html.classList.remove('scroll-smooth')
-        }
-        router.events.on('routeChangeStart', removeSmoothScrollClass)
-        router.events.on('routeChangeComplete', addSmoothScrollClass)
-        router.events.on('routeChangeError', addSmoothScrollClass)
-        return () => {
-            router.events.off('routeChangeStart', removeSmoothScrollClass)
-            router.events.off('routeChangeComplete', addSmoothScrollClass)
-            router.events.off('routeChangeError', addSmoothScrollClass)
-        }
-    }, [router])
+    // useEffect(() => {
+    //     const html = document.querySelector('html')
+    //     const addSmoothScrollClass = () => {
+    //         html.classList.add('scroll-smooth')
+    //     }
+    //     const removeSmoothScrollClass = () => {
+    //         html.classList.remove('scroll-smooth')
+    //     }
+    //     router.events.on('routeChangeStart', removeSmoothScrollClass)
+    //     router.events.on('routeChangeComplete', addSmoothScrollClass)
+    //     router.events.on('routeChangeError', addSmoothScrollClass)
+    //     return () => {
+    //         router.events.off('routeChangeStart', removeSmoothScrollClass)
+    //         router.events.off('routeChangeComplete', addSmoothScrollClass)
+    //         router.events.off('routeChangeError', addSmoothScrollClass)
+    //     }
+    // }, [router])
 
     // Use the layout defined at the page level, if available
     // const Layout = Component.layout || (({ children }) => (<RootLayout>{children}</RootLayout>))
@@ -130,14 +124,18 @@ export default function App({ Component, pageProps }) {
                 </style>
                 {draftMode ? (
                     <PreviewProvider token={token}>
-                        <RootLayout>
-                            <Component {...pageProps} key={key} />
-                        </RootLayout>
+                            <RootLayout>
+                                <PageTransition disableDefaultStyles inPhaseDuration={100} outPhaseDuration={100}>
+                                    <Component {...pageProps} key={key} />
+                                </PageTransition>
+                            </RootLayout>
                     </PreviewProvider>
-                    ) : (
-                        <RootLayout>
+                ) : (
+                    <RootLayout>
+                        <PageTransition disableDefaultStyles inPhaseDuration={100} outPhaseDuration={100}>
                             <Component {...pageProps} key={key} />
-                        </RootLayout>
+                        </PageTransition>
+                    </RootLayout>
                 )}
             </HydrationProvider>
 		</>
