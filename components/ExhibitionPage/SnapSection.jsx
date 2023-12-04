ea0b9c50ca1621d8650bc809dbf9dc5f61655532
Henry Babbage <hbabbage@gmail.com>
2023-12-04 20:00:35 +0100

    remove unused useeffect

--- components/ExhibitionPage/SnapSection.jsx
+++ components/ExhibitionPage/SnapSection.jsx
@@ -1,22 +1,11 @@
-import { forwardRef, Fragment, useEffect } from 'react'
+import { forwardRef, Fragment } from 'react'
 import { useInView } from 'react-intersection-observer'
-
-import { useActiveImageStore } from '@/context/useActiveImageStore'
 
 import AspectImage from '../Common/Media/AspectImage'
 import FullBleedImage from '../Common/Media/FullBleedImage'
 
 const SnapSection = forwardRef(function SnapSection({ exhibition, scrollToSections, index }, ref) {
 	const { ref: inViewRef, inView } = useInView()
-	const setInViewImage = useActiveImageStore((state) => state.setInViewImage)
-	const inViewImage = useActiveImageStore((state) => state.inViewImage)
-
-	useEffect(() => {
-		if (inView) {
-			console.log(inView)
-        }
-	}, [index, inView, setInViewImage])
-
   	return (
 		<Fragment ref={ref}>
 			<section ref={inViewRef}
