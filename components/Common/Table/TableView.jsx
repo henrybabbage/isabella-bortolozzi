b548a8a80e30b6b01e7312d656d2d93356880a87
Henry Babbage <hbabbage@gmail.com>
2023-12-09 11:37:39 +0100

    fix responsiveness of table on mobile

--- components/Common/Table/TableView.jsx
+++ components/Common/Table/TableView.jsx
@@ -16,10 +16,11 @@ export default function TableView({ exhibitions }) {
     const parentRef = useRef(null)
     const tableContentRef = useRef(null)
     const listRef = useRef(null)
+    const virtualItemSize = tabletOrMobile ? 640 : 312
 
     const virtualizer = useWindowVirtualizer({
         count: exhibitions.length,
-        estimateSize: () => 312,
+        estimateSize: () => virtualItemSize,
         overscan: 8,
         scrollMargin: listRef.current?.offsetTop ?? 0,
     })
@@ -49,44 +50,33 @@ export default function TableView({ exhibitions }) {
 				</div>
 			</div>
 			<div ref={listRef} className={cn("", "scrollbar-hide sm:col-span-9 sm:col-start-4 col-start-1 col-span-12 w-full py-[calc(50vh-11vw)]")}>
-                {tabletOrMobile ? (
-                    <ol ref={tableContentRef}>
-                        {exhibitions &&
-                            exhibitions.map((exhibition) => (
-                                <li key={exhibition._id} id={exhibition.year} className="scroll-mt-[calc(50vh-11vw)]">
-                                    <TableItem id={exhibition._id} year={exhibition.year} exhibition={exhibition} />
+                <ol
+                    ref={tableContentRef}
+                    style={{
+                        height: `${virtualizer.getTotalSize()}px`,
+                        width: '100%',
+                        position: 'relative',
+                    }}
+                >
+                    {virtualizer.getVirtualItems().map((item) => {
+                        return (
+                            <div
+                                key={item.key}
+                                style={{
+                                    position: 'absolute',
+                                    top: 0,
+                                    left: 0,
+                                    width: '100%',
+                                    height: `${item.size}px`,
+                                    transform: `translateY(${item.start - virtualizer.options.scrollMargin}px)`,
+                                }}
+                            >
+                                <li className='scroll-mt-[calc(50vh-11vw)]'>
+                                    <TableItem exhibition={exhibitions[item.index]} />
                                 </li>
-                            ))}
-                    </ol>
-                ) : (
-                    <ol
-                        ref={tableContentRef}
-                        style={{
-                            height: `${virtualizer.getTotalSize()}px`,
-                            width: '100%',
-                            position: 'relative',
-                        }}
-                    >
-                        {virtualizer.getVirtualItems().map((item) => {
-                            return (
-                                <div
-                                    key={item.key}
-                                    style={{
-                                        position: 'absolute',
-                                        top: 0,
-                                        left: 0,
-                                        width: '100%',
-                                        height: `${item.size}px`,
-                                        transform: `translateY(${item.start - virtualizer.options.scrollMargin}px)`,
-                                    }}
-                                >
-                                    <li className='scroll-mt-[calc(50vh-11vw)]'>
-                                        <TableItem exhibition={exhibitions[item.index]} />
-                                    </li>
-                                </div>
-                        )})}
-                    </ol>
-                )}
+                            </div>
+                    )})}
+                </ol>
 			</div>
 		</div>
 	)
