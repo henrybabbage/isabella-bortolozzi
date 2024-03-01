e6f5a8b0decee6bf76e42cfe19e3ee5f1116ab70
Henry Babbage <hbabbage@gmail.com>
2024-03-01 17:35:46 +0100

    return smooth scroll to exhibition

--- components/Layout/ExhibitionLayout.jsx
+++ components/Layout/ExhibitionLayout.jsx
@@ -9,7 +9,7 @@ export default function ExhibitionLayout({ children }) {
   const router = useRouter()
   return (
     <div id="exhibition" className="scrollbar-hide overscroll-none">
-      {/* <SmoothScroll> */}
+      <SmoothScroll>
         <Client>
           <div className="fixed top-6 right-6 z-50">
             <BackButton backPathname={router.pathname.split('/')[1]} />
@@ -17,7 +17,7 @@ export default function ExhibitionLayout({ children }) {
           <ReferenceGrid />
         </Client>
         {children}
-      {/* </SmoothScroll> */}
+      </SmoothScroll>
     </div>
   )
 }
--- components/Layout/RootLayout.jsx
+++ components/Layout/RootLayout.jsx
@@ -6,31 +6,32 @@ import { Desktop, TabletAndBelow } from '@/utils/breakpoints'
 import GlobalHeader from '../Common/Drawers/GlobalHeader'
 import MobileHeader from '../Mobile/MobileHeader'
 import ReferenceGrid from '../Utilities/ReferenceGrid'
+import SmoothScroll from '../Utilities/SmoothScroll'
 
 export default function RootLayout({ children }) {
   const router = useRouter()
   return (
     <div id="root" className="-z-0 overscroll-none">
-      {/* <SmoothScroll> */}
-      <Client>
-        <Desktop>
-          {!router.pathname.startsWith('/studio') &&
-          !router.pathname.startsWith('/exhibitions/') &&
-          !router.pathname.startsWith('/viewing-rooms/') ? (
-            <GlobalHeader isFixed={true} />
-          ) : null}
-        </Desktop>
-        <TabletAndBelow>
-          {!router.pathname.startsWith('/studio') &&
-          !router.pathname.startsWith('/exhibitions/') &&
-          !router.pathname.startsWith('/viewing-rooms/') ? (
-            <MobileHeader isFixed={true} />
-          ) : null}
-        </TabletAndBelow>
-        <ReferenceGrid />
-      </Client>
-      {children}
-      {/* </SmoothScroll> */}
+      <SmoothScroll>
+        <Client>
+          <Desktop>
+            {!router.pathname.startsWith('/studio') &&
+            !router.pathname.startsWith('/exhibitions/') &&
+            !router.pathname.startsWith('/viewing-rooms/') ? (
+              <GlobalHeader isFixed={true} />
+            ) : null}
+          </Desktop>
+          <TabletAndBelow>
+            {!router.pathname.startsWith('/studio') &&
+            !router.pathname.startsWith('/exhibitions/') &&
+            !router.pathname.startsWith('/viewing-rooms/') ? (
+              <MobileHeader isFixed={true} />
+            ) : null}
+          </TabletAndBelow>
+          <ReferenceGrid />
+        </Client>
+        {children}
+      </SmoothScroll>
     </div>
   )
 }
