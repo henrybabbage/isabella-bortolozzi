69253ee924633ea0a5cecf91eaed233fb0ece77d
Henry Babbage <hbabbage@gmail.com>
2024-03-01 17:17:24 +0100

    tidy up current page placeholder

--- pages/current.jsx
+++ pages/current.jsx
@@ -1,7 +1,7 @@
 export default function CurrentExhibition() {
   return (
-    <div className="animate-fade-in">
-      <h1 className="font-serif text-primary">Current</h1>
+    <div className="animate-fade-in h-screen w-full flex flex-col justify-center items-center">
+      <h1 className="font-serif text-primary text-sm uppercase">Current</h1>
     </div>
   )
 }
