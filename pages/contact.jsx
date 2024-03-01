69585efc5e80d03a3fc7f9ceb55b9cfd68ab90cf
Henry Babbage <hbabbage@gmail.com>
2024-03-01 17:18:19 +0100

    add contact page placeholder

---
+++ pages/contact.jsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+  return (
+    <div className="animate-fade-in h-screen w-full flex flex-col justify-center items-center">
+      <h1 className="font-serif text-primary text-sm uppercase">Contact</h1>
+    </div>
+  )
+}
