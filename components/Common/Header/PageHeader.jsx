7d388f215eec4b082eddae188f2478f4b90b7422
Henry Babbage <hbabbage@gmail.com>
2024-03-01 19:47:53 +0100

    refactor nav based on florian's design

--- components/Common/Header/PageHeader.jsx
+++ components/Common/Header/PageHeader.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function PageHeader({ title, children }) {
   return (
     <section className="fixed bg-background h-auto z-50 top-0 grid w-screen grid-cols-12 px-6 pt-6 pb-1 sm:pt-0 sm:pb-0">
-      <div className="pb-2 sm:col-span-9 sm:col-start-2 col-span-12 col-start-1 flex justify-between sm:justify-normal w-full space-x-12 pt-14 sm:pt-6">
+      <div className="pb-2 sm:col-span-9 sm:col-start-2 col-span-12 col-start-1 flex justify-between sm:justify-normal w-full space-x-12 pt-14 sm:pt-2">
         <h1 className="text-primary">{title}</h1>
         {children}
       </div>
