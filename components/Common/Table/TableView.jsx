663a8e2d84aa8afce1d56a2ed15e08846f70086b
Henry Babbage <hbabbage@gmail.com>
2024-03-02 13:47:16 +0100

    remove padding top

--- components/Common/Table/TableView.jsx
+++ components/Common/Table/TableView.jsx
@@ -79,7 +79,7 @@ export default function TableView({ exhibitions }) {
     estimateSize: () => virtualItemSize,
     overscan: 8,
     scrollMargin: listRef?.current?.offsetTop ?? 0,
-    paddingStart: 64,
+    // paddingStart: 64,
     // paddingEnd: 64,
   })
 
