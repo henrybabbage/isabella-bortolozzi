1aba0a9e9e85a21c3be2e93b24be4d032efdfd0e
Henry Babbage <hbabbage@gmail.com>
2024-03-05 20:26:34 +0100

    wip work on pre build step fetch

--- functions/preBuildUtilities.js
+++ functions/preBuildUtilities.js
@@ -5,28 +5,30 @@ const PROJECT_ID = 'yy07oxit'
 const DATASET = 'production'
 const API_VERSION = 'v2024-01-09'
 
-const ARTISTS_QUERY = `*[_type == "artist" && galleryArtist == true] | order(orderRank)
+const ARTISTS_QUERY = `*[_type == "artist" && $galleryArtist == $bool] | order(orderRank)
     {
         name,
         "slug": slug.current,
+        galleryArtist,
     }
 `
 
 module.exports.preBuildDevelopment = async () => {
   console.log('Loading the development content!')
-  const API = `https://${PROJECT_ID}.api.sanity.io/${API_VERSION}/data/query/${DATASET}?query=${ARTISTS_QUERY}`
+  const API = `https://${PROJECT_ID}.api.sanity.io/${API_VERSION}/data/query/${DATASET}?query=${ARTISTS_QUERY}&$galleryArtist='galleryArtist'&$bool=true`
   const response = await axios.get(API)
-  const data = response.data.result.menuItems
-  fs.writeFileSync('./data/menu.json', JSON.stringify(data))
+  const data = response.data.result
+  console.log(data)
+  fs.writeFileSync('./data/nav.json', JSON.stringify(data))
 }
 
 module.exports.preBuildProduction = async () => {
   console.log('Loading the production content!')
-  const API = `https://${PROJECT_ID}.api.sanity.io/${API_VERSION}/data/query/${DATASET}?query=${ARTISTS_QUERY}`
+  const API = `https://${PROJECT_ID}.api.sanity.io/${API_VERSION}/data/query/${DATASET}?query=${ARTISTS_QUERY}&$galleryArtist='galleryArtist'&$bool=true`
   const response = await axios.get(API)
-  const data = response.data.result.menuItems
-  fs.writeFileSync('./data/menu.json', JSON.stringify(data))
+  const data = response.data.result
+  fs.writeFileSync('./data/nav.json', JSON.stringify(data))
 }
 
-// "prebuild": "node functions/preBuildProd.js",
 // "predev": "node functions/preBuildDev.js",
+// "prebuild": "node functions/preBuildProd.js",
