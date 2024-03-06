e057fae71f3d21bc3f08838adcb0ea5044da7b4f
Henry Babbage <hbabbage@gmail.com>
2024-03-06 12:15:55 +0100

    script returns all artists but params not working yet

--- functions/preBuildUtilities.js
+++ functions/preBuildUtilities.js
@@ -5,13 +5,16 @@ const PROJECT_ID = 'yy07oxit'
 const DATASET = 'production'
 const API_VERSION = 'v2024-01-09'
 
-const ARTISTS_QUERY = `*[_type == "artist"]{name,"slug":slug.current,galleryArtist}`
+const ARTISTS_QUERY = `*[_type == "artist"]{name,"slug":slug.current}`
 
 module.exports.preBuildDevelopment = async () => {
   console.log('Loading the development content!')
   const QUERY = encodeURI(
     `https://${PROJECT_ID}.api.sanity.io/${API_VERSION}/data/query/${DATASET}?query=${ARTISTS_QUERY}`,
   )
+  // const galleryArtist = encodeURI(`$galleryArtist="galleryArtist"`)
+  // const bool = encodeURI(`$bool=true`)
+  // const API = `${QUERY}&${galleryArtist}&${bool}`
   const response = await axios.get(QUERY)
   const data = response.data.result
   console.log(data)
