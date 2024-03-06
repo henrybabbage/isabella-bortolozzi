597f507c498ecd5523f2b14b416bc9ee501d2cb2
Henry Babbage <hbabbage@gmail.com>
2024-03-06 12:01:42 +0100

    simple query without params runs

--- functions/preBuildUtilities.js
+++ functions/preBuildUtilities.js
@@ -1,5 +1,21 @@
+const fs = require('fs')
+const axios = require('axios').default
+
+const PROJECT_ID = 'yy07oxit'
+const DATASET = 'production'
+const API_VERSION = 'v2024-01-09'
+
+const ARTISTS_QUERY = `*[_type == "artist"]{name,"slug":slug.current,galleryArtist}`
+
 module.exports.preBuildDevelopment = async () => {
   console.log('Loading the development content!')
+  const QUERY = encodeURI(
+    `https://${PROJECT_ID}.api.sanity.io/${API_VERSION}/data/query/${DATASET}?query=${ARTISTS_QUERY}`,
+  )
+  const response = await axios.get(QUERY)
+  const data = response.data.result
+  console.log(data)
+  fs.writeFileSync('./data/nav.json', JSON.stringify(data))
 }
 
 module.exports.preBuildProduction = async () => {
