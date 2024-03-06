1a2ea9742014900a636f38bfdce343382c393b51
Henry Babbage <hbabbage@gmail.com>
2024-03-06 11:58:24 +0100

    set up pre dev scripts from scratch again

---
+++ functions/preBuildUtilities.js
@@ -0,0 +1,7 @@
+module.exports.preBuildDevelopment = async () => {
+  console.log('Loading the development content!')
+}
+
+module.exports.preBuildProduction = async () => {
+  console.log('Loading the production content!')
+}
