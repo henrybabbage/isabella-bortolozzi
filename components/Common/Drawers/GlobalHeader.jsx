dc515f6190934292516a9f77ed4d6da7b7a793ec
Henry Babbage <hbabbage@gmail.com>
2024-03-08 12:50:20 +0100

    tidy up

--- components/Common/Drawers/GlobalHeader.jsx
+++ components/Common/Drawers/GlobalHeader.jsx
@@ -101,7 +101,6 @@ export default function GlobalHeader({ isFixed = true }) {
           <h1 className="text-primary pt-2">Artists</h1>
         </div>
       </div>
-      {/* Fix this overlapping page content when hidden */}
       <nav
         className={cn(
           isNavOpen
