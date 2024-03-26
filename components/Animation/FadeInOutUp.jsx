ce94d1abc9cc33aac7dd51b86f05976166ea450e
Henry Babbage <hbabbage@gmail.com>
2024-03-26 18:05:20 +0100

    normalize

--- components/Animation/FadeInOutUp.jsx
+++ components/Animation/FadeInOutUp.jsx
@@ -1,26 +1,32 @@
 import AnimateInOut from './AnimateInOut'
 
-const FadeInOutUp = ({ children, delay, y, durationIn, durationOut }) => (
-  <AnimateInOut
-    as="div"
-    durationIn={durationIn || 2}
-    durationOut={durationOut || 0.25}
-    delay={delay}
-    from={{
-      transform: 'translate(0px, ' + (y || 40) + 'px)',
-      opacity: 0,
-      duration: 0.25,
-      ease: 'power4.out',
-    }}
-    to={{
-      opacity: 1,
-      y: 0,
-      ease: 'power4.out',
-      delay: 0.5,
-    }}
-  >
-    {children}
-  </AnimateInOut>
-)
-
-export default FadeInOutUp
+export default function FadeInOutUp({
+  children,
+  delay,
+  y,
+  durationIn,
+  durationOut,
+}) {
+  return (
+    <AnimateInOut
+      as="div"
+      durationIn={durationIn || 2}
+      durationOut={durationOut || 0.25}
+      delay={delay}
+      from={{
+        transform: 'translate(0px, ' + (y || 40) + 'px)',
+        opacity: 0,
+        duration: 0.25,
+        ease: 'power4.out',
+      }}
+      to={{
+        opacity: 1,
+        y: 0,
+        ease: 'power4.out',
+        delay: 0.5,
+      }}
+    >
+      {children}
+    </AnimateInOut>
+  )
+}
