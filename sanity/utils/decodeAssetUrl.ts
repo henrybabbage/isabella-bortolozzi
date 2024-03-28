3460ba8d3607a127b0c0e70cfe1c7ecdcd9d155d
Henry Babbage <hbabbage@gmail.com>
2024-03-28 12:57:26 +0100

    fix type erorr

--- sanity/utils/decodeAssetUrl.ts
+++ sanity/utils/decodeAssetUrl.ts
@@ -1,14 +1,19 @@
 export const decodeAssetUrl = (id: string) => {
-    const pattern = /^(?:image|file)-([a-f\d]+)-(?:(\d+x\d+)-)?(\w+)$/
-    const [assetId, dimensions, format] = pattern.exec(id)
-  
-    const [width, height] = dimensions
-      ? dimensions.split('x').map(v => parseInt(v, 10))
-      : []
-  
-    return {
-      assetId,
-      dimensions: { width, height },
-      format
-    }
+  const pattern = /^(?:image|file)-([a-f\d]+)-(?:(\d+x\d+)-)?(\w+)$/
+  const match = pattern.exec(id)
+  if (!match) {
+    throw new Error('Invalid asset URL')
+  }
+
+  const [assetId, dimensions, format] = match
+
+  const [width, height] = dimensions
+    ? dimensions.split('x').map((v) => parseInt(v, 10))
+    : []
+
+  return {
+    assetId,
+    dimensions: { width, height },
+    format,
+  }
 }
