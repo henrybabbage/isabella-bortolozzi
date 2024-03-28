9e00bad932b2b9de249ee2a7e64193a62c8d15a1
Henry Babbage <hbabbage@gmail.com>
2024-03-28 20:18:58 +0100

    finally resolve the highlight style for the table grid by setting grid-template-rows-[10.25vw] for height to highlight

--- components/Common/Table/TableItem.jsx
+++ components/Common/Table/TableItem.jsx
@@ -32,7 +32,7 @@ const TableItem = forwardRef(function TableItem({ exhibition, index }, ref) {
     <DynamicLink link={exhibition} scroll={false}>
       <div
         ref={ref}
-        className="text-primary transition-colors duration-700 ease-in-out group relative flex flex-col sm:grid h-[40rem] sm:h-[10.25vw] sm:max-h-[10.25vw] cursor-pointer sm:grid-cols-6 px-4 content-start pt-1 sm:pt-0 sm:pb-6 text-left"
+        className="text-primary grid-rows-[10.25vw] transition-colors duration-700 ease-in-out group relative flex flex-col sm:grid h-[40rem] sm:h-[10.25vw] sm:max-h-[10.25vw] cursor-pointer sm:grid-cols-6 px-4 content-start pt-1 sm:pt-0 sm:pb-6 text-left"
       >
         <TabletAndBelow>
           <div className="sm:hidden h-[22rem] max-h-full w-full overflow-hidden pb-6">
